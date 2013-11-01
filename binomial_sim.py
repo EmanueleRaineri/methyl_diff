2d0dc3583ee9e230f195e469fc46d9a6f9314445
Emanuele Raineri <emanuele.raineri@gmail.com>
2013-11-01 18:56:16 +0100

    binomial simulator

---
+++ binomial_sim.py
@@ -0,0 +1,18 @@
+import sys
+import scipy.stats
+
+
+cov1 , p1 , cov2, p2, size = [float(e) for e in sys.argv[1:]]
+size=int(size)
+"""beta1=scipy.stats.beta( a1 , b1 )
+beta2=scipy.stats.beta( a2 , b2 )"""
+binom1=scipy.stats.binom(cov1,p1)
+binom2=scipy.stats.binom(cov1,p1)
+
+for i in range(size):
+    nc1=binom1.rvs()
+    c1=int(cov1-nc1)
+    nc2=binom2.rvs()
+    c2=int(cov2-nc2)
+    print nc1 , c1 , nc2, c2 
+
