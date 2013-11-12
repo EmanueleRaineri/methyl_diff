e1085e937b877a569f1970513b2d9635a1240b74
Emanuele Raineri <emanuele.raineri@gmail.com>
2013-11-12 11:19:58 +0100

    added z score to plot

--- cmp_power.sh
+++ cmp_power.sh
@@ -12,16 +12,28 @@ echo "fisher on tp..."
 python fisher.py < tp.txt > fisher_of_tp_${COV1}_${COV2}.txt
 echo "fisher on tn..."
 python fisher.py < tn.txt > fisher_of_tn_${COV1}_${COV2}.txt
+#######
 echo "beta_diff on tp"
 awk '{print $1+1,$2+1,$3+1,$4+1}' tp.txt | ocaml str.cma beta_diff.ml > beta_of_tp_${COV1}_${COV2}.txt
 echo "beta_diff on tn"
 awk '{print $1+1,$2+1,$3+1,$4+1}' tn.txt | ocaml str.cma beta_diff.ml > beta_of_tn_${COV1}_${COV2}.txt
 #######
+echo "Z score on tp"
+python zscore.py < tp.txt |awk '{print $2}' > zscore_of_tp_${COV1}_${COV2}.txt
+python zscore.py < tn.txt |awk '{print $2}' > zscore_of_tn_${COV1}_${COV2}.txt
+#######
+echo "compute power"
+#fisher
 python compute_power.py fisher_of_tp_${COV1}_${COV2}.txt fisher_of_tn_${COV1}_${COV2}.txt > fisher.roc.${COV1}_${COV2}.txt
+#z score
+python compute_power.py zscore_of_tp_${COV1}_${COV2}.txt zscore_of_tn_${COV1}_${COV2}.txt > zscore.roc.${COV1}_${COV2}.txt
+#beta
 python compute_power_beta.py beta_of_tp_${COV1}_${COV2}.txt beta_of_tn_${COV1}_${COV2}.txt > beta.roc.${COV1}_${COV2}.txt
-paste fisher.roc.${COV1}_${COV2}.txt beta.roc.${COV1}_${COV2}.txt > roc_${COV1}_${COV2}.txt
+#######
+paste fisher.roc.${COV1}_${COV2}.txt beta.roc.${COV1}_${COV2}.txt zscore.roc.${COV1}_${COV2}.txt > roc_${COV1}_${COV2}.txt
 cat fisher_of_tp_${COV1}_${COV2}.txt fisher_of_tn_${COV1}_${COV2}.txt > fisher_${COV1}_${COV2}.txt 
 cat beta_of_tp_${COV1}_${COV2}.txt beta_of_tn_${COV1}_${COV2}.txt > beta_${COV1}_${COV2}.txt 
-paste fisher_${COV1}_${COV2}.txt beta_${COV1}_${COV2}.txt > fisher_beta_${COV1}_${COV2}.txt
-#plot with plot_roc.py 
+cat zscore_of_tp_${COV1}_${COV2}.txt zscore_of_tn_${COV1}_${COV2}.txt > zscore_${COV1}_${COV2}.txt 
+paste fisher_${COV1}_${COV2}.txt beta_${COV1}_${COV2}.txt zscore_${COV1}_${COV2}.txt > fisher_beta_zscore_${COV1}_${COV2}.txt
 #######
+#plot with plot_roc.py fisher.roc zscore.roc beta.roc
