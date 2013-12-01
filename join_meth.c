7205919768caca9a64822cfcdcc116e208a95355
Emanuele Raineri <emanuele.raineri@gmail.com>
2013-12-01 10:22:23 +0100

    boh

--- join_meth.c
+++ join_meth.c
@@ -11,6 +11,11 @@ int index_chr(char* c, char** chrnames, int n){
     return -1;
 }
 
+
+int max(int a, int b){
+    if (a>b) return a; else return b;
+
+}
 int main(int argc, char* argv[]){
     if (argc<4) {
         fprintf(stderr,"usage: <chr file> <methyl1> <methyl2>\n");
@@ -51,18 +56,35 @@ int main(int argc, char* argv[]){
     char* sep ="\\t ";
     char* word;
     char *chr1,*chr2;
-    int pos1,pos2,i1,i2;
+    int pos1,pos2,i1,i2,mini,minpos;
     chr1=malloc(100);
     chr2=malloc(100);
+    line1 = fgets( line1, 1000, f1  );
+    line2 = fgets( line2, 1000, f2  );
+    if ( feof(f1) || feof(f2) ) exit(0);
+    sscanf( line1 , "%s %d" , chr1 , &pos1 ); 
+    sscanf( line2 , "%s %d" , chr2 , &pos2 ); 
+    i1 = index_chr( chr1 , chrnames , c );
+    i2 = index_chr( chr2 , chrnames , c );
+    maxi = max(i1,i2);
+    maxpos = max(pos1,pos2);
     while(1){
-        line1 = fgets(line1, 1000, f1  );
-        line2 = fgets(line2, 1000, f2  );
-        if ( feof(f1) || feof(f2) ) break;
-        sscanf( line1 , "%s %d" , chr1 , &pos1 ); 
-        sscanf( line2 , "%s %d" , chr2 , &pos2 ); 
-        fprintf(stdout,"%s %d %s %d\n",chr1,pos1,chr2,pos2);
-        i1=index_chr(chr1,chrnames,c);
-        i2=index_chr(chr2,chrnames,c);
+        fprintf(stderr,"maxi:%d,maxpos:%d line1:%s line2:%s",
+            maxi,maxpos,line1,line2);
+        if ( i1 == i2 && pos1 == pos2 ) fprintf(stdout,"%s\t%s\n",line1,line2);
+        if (i1 < maxi ){
+            line1 = fgets( line1, 1000, f1  );
+            if ( feof( f1 )  ) break;
+            sscanf( line1 , "%s %d" , chr1 , &pos1 ); 
+        }
+        if ( i1 == maxi && pos1 < pos2 ){
+             
+        }
+        //fprintf(stdout,"%s %d %s %d\n",chr1,pos1,chr2,pos2);
+        i1 = index_chr( chr1 , chrnames , c );
+        i2 = index_chr( chr2 , chrnames , c );
         //fprintf(stdout,"%d %d\n",i1,i2);
+        maxi   = max(i1,i2);
+        maxpos = max(pos1,pos2);
     }
 }
